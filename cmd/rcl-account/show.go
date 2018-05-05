fb75ccba6372bd2c6b52b71941878cf4f1bd238d
Dave Cohen <git@dave-cohen.com>
2018-05-05 10:44:53 -0700

    rcl-account show: rendering tweak and remove obsolete code

--- cmd/rcl-account/show.go
+++ cmd/rcl-account/show.go
@@ -129,7 +129,7 @@ func (s *State) showCommand(fs *flag.FlagSet) {
 		table := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.Debug)
 		fmt.Fprintln(table, "Account\t XRP\t Sequence\t Owner Count\t Ledger Index\t")
 		fmt.Fprintf(table, "%s\t %s\t %d\t %d\t %d\t\n",
-			account,
+			config.FormatAccountName(*account),
 			accountResult.AccountData.Balance,
 			*accountResult.AccountData.Sequence,
 			*accountResult.AccountData.OwnerCount,
@@ -138,16 +138,16 @@ func (s *State) showCommand(fs *flag.FlagSet) {
 		table.Flush()
 		fmt.Println("") // blank line
 
-		table = tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.DiscardEmptyColumns)
+		table = tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.DiscardEmptyColumns|tabwriter.Debug)
 		fmt.Fprintln(table, "Balances\t Amount\t Currency/Issuer\t Min\t Max\t rippling\t quality\t")
-		fmt.Fprintf(table, "%s\t %s\t %s\t\t\t\t\t\t\n", account, accountResult.AccountData.Balance, "XRP")
+		fmt.Fprintf(table, "%s\t %s\t %s\t\t\t\t\t\n", config.FormatAccountName(*account), accountResult.AccountData.Balance, "XRP")
 		for _, line := range linesResults[key].Lines {
 			peerLimit, err := line.LimitPeer.Multiply(*minusOne)
 
 			if err != nil {
 				log.Panic(err)
 			}
-			fmt.Fprintf(table, "%s\t %s\t %s/%s\t %s\t %s\t %s\t %s\t\n", account, line.Balance, line.Currency, line.Account, peerLimit, line.Limit, formatRipple(line), formatQuality(line))
+			fmt.Fprintf(table, "%s\t %s\t %s/%s\t %s\t %s\t %s\t %s\t\n", config.FormatAccountName(*account), line.Balance, line.Currency, line.Account, peerLimit, line.Limit, formatRipple(line), formatQuality(line))
 			//q.Q(line)
 		}
 		table.Flush()
@@ -204,7 +204,7 @@ func (s *State) showCommand(fs *flag.FlagSet) {
 				return book[bidOrAsk][i].offer.Quality.Less(book[bidOrAsk][j].offer.Quality.Value)
 			})
 		}
-		table := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.DiscardEmptyColumns)
+		table := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.DiscardEmptyColumns|tabwriter.Debug)
 		// TODO add currencies to header
 		fmt.Fprintln(table, "Bid by (Sequence)\t TakerPays\t Price\t Price\t TakerGets\t Ask by (Sequence)")
 		row := 0
@@ -212,7 +212,7 @@ func (s *State) showCommand(fs *flag.FlagSet) {
 			if row < len(book[0]) {
 				offer := book[0][row]
 				price := offer.offer.TakerGets.Ratio(offer.offer.TakerPays)
-				fmt.Fprintf(table, "%s(%d)\t %s\t %.4f\t", offer.account, offer.offer.Sequence, offer.offer.TakerPays, price.Float())
+				fmt.Fprintf(table, "%s (%d)\t %s\t %.4f\t", config.FormatAccountName(offer.account), offer.offer.Sequence, offer.offer.TakerPays, price.Float())
 			} else {
 				fmt.Fprintf(table, "n/a \t \t \t")
 			}
@@ -220,7 +220,7 @@ func (s *State) showCommand(fs *flag.FlagSet) {
 			if row < len(book[1]) {
 				offer := book[1][row]
 				price := offer.offer.TakerPays.Ratio(offer.offer.TakerGets)
-				fmt.Fprintf(table, "%.4f\t %s\t %s(%d)\t\n", price.Float(), offer.offer.TakerGets, offer.account, offer.offer.Sequence)
+				fmt.Fprintf(table, "%.4f\t %s\t %s (%d)\t\n", price.Float(), offer.offer.TakerGets, config.FormatAccountName(offer.account), offer.offer.Sequence)
 			} else {
 				fmt.Fprintf(table, "\t \t n/a\t\n")
 			}
@@ -229,59 +229,6 @@ func (s *State) showCommand(fs *flag.FlagSet) {
 		table.Flush()
 		fmt.Println("") // blank line
 	}
-
-	// devel
-	if true {
-		return
-	}
-	// Render all offers...
-	// sort all offers into human-readable order
-	type mapped struct {
-		offer   data.AccountOffer
-		account data.Account
-	}
-	byKey := make(map[string]mapped)
-	for _, account := range accounts {
-
-		if offerResults[account] == nil {
-			// Account not found
-			continue
-		}
-
-		for _, offer := range offerResults[account].Offers {
-			// sortable key
-			key1 := fmt.Sprintf("%s/%s/bid/%s", offer.TakerPays.Asset(), offer.TakerGets.Asset(), offer.Quality) // Arbitrarily call one side the bid
-			key2 := fmt.Sprintf("%s/%s/ask/%s", offer.TakerGets.Asset(), offer.TakerPays.Asset(), offer.Quality) // the other side is ask.
-			// choose key so that bids and asks are next to each other in final ordering
-			key := key1
-			if strings.Compare(key1, key2) == -1 {
-				key = key2
-			}
-			byKey[key] = mapped{
-				offer:   offer,
-				account: *account,
-			}
-		}
-	}
-
-	if len(byKey) > 0 {
-		table := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.DiscardEmptyColumns)
-		fmt.Fprintln(table, "Offers\t Sequence\t TakerGets\t TakerPays\t Price\t")
-
-		allKeys := make([]string, 0, len(byKey))
-		for k, _ := range byKey {
-			allKeys = append(allKeys, k)
-		}
-		sort.Strings(allKeys)
-		for _, k := range allKeys {
-			v := byKey[k]
-			price := v.offer.TakerPays.Ratio(v.offer.TakerGets)
-			fmt.Fprintf(table, "%s\t %d\t %s\t %s\t %s\n", v.account, v.offer.Sequence, v.offer.TakerGets, v.offer.TakerPays, price)
-		}
-		table.Flush()
-		fmt.Println("") // blank line
-	}
-
 }
 
 func formatRipple(line data.AccountLine) string {
