76f87c1f1dea8589292155f3f32ff7571d68fa98
Dave Cohen <git@dave-cohen.com>
2018-10-10 16:11:07 -0700

    rippledata: helper class to iterate over historic activity

--- rippledata/transaction.go
+++ rippledata/transaction.go
@@ -38,16 +38,74 @@ func (this TransactionResponse) GetTransactionIndex() uint32 {
 	return 0
 }
 
-type TransactionQueue []Transaction
+// Helper type for TransactionFIFO and TransactionLIFO
+type transactionHeap []Transaction
 
-func NewTransactionQueue() *TransactionQueue {
-	tq := make(TransactionQueue, 0, 1)
+func newTransactionQueue() *transactionHeap {
+	tq := make(transactionHeap, 0, 1)
 	return &tq
 }
 
-func (this TransactionQueue) Len() int { return len(this) }
+func (this transactionHeap) Len() int { return len(this) }
 
-func (this TransactionQueue) Less(i, j int) bool {
+func (this transactionHeap) Swap(i, j int) {
+	this[i], this[j] = this[j], this[i]
+}
+
+// Push and Pop use pointer receivers because they modify slice's length
+func (this *transactionHeap) Push(x interface{}) {
+	*this = append(*this, x.(Transaction))
+}
+
+func (this *transactionHeap) Pop() interface{} {
+	old := *this
+	n := len(old)
+	item := old[n-1]
+	*this = old[0 : n-1]
+	return item
+}
+
+type TransactionLIFO transactionHeap
+
+func NewTransactionLIFO() *TransactionLIFO {
+	return (*TransactionLIFO)(newTransactionQueue())
+}
+
+// Less determines whether heap is LIFO or FIFO
+func (this TransactionLIFO) Less(i, j int) bool {
+	if this[i].GetLedgerIndex() > this[j].GetLedgerIndex() {
+		return true
+	} else if this[i].GetLedgerIndex() == this[j].GetLedgerIndex() {
+		// Unfortunately tx_index is not returned from all APIs.  It is returned from balance_changes.
+		return this[i].GetTransactionIndex() > this[j].GetTransactionIndex()
+	}
+	return false
+}
+
+func (this TransactionLIFO) Len() int {
+	return transactionHeap(this).Len()
+}
+
+func (this TransactionLIFO) Swap(i, j int) {
+	transactionHeap(this).Swap(i, j)
+}
+
+// Push and Pop use pointer receivers because they modify slice's length
+func (this *TransactionLIFO) Push(x interface{}) {
+	(*transactionHeap)(this).Push(x)
+}
+func (this *TransactionLIFO) Pop() interface{} {
+	return (*transactionHeap)(this).Pop()
+}
+
+type TransactionFIFO transactionHeap
+
+func NewTransactionFIFO() *TransactionFIFO {
+	return (*TransactionFIFO)(newTransactionQueue())
+}
+
+// Less determines whether heap is LIFO or FIFO.  Pay attention to less than.
+func (this TransactionFIFO) Less(i, j int) bool {
 	if this[i].GetLedgerIndex() < this[j].GetLedgerIndex() {
 		return true
 	} else if this[i].GetLedgerIndex() == this[j].GetLedgerIndex() {
@@ -57,19 +115,18 @@ func (this TransactionQueue) Less(i, j int) bool {
 	return false
 }
 
-func (this TransactionQueue) Swap(i, j int) {
-	this[i], this[j] = this[j], this[i]
+func (this TransactionFIFO) Len() int {
+	return transactionHeap(this).Len()
+}
+
+func (this TransactionFIFO) Swap(i, j int) {
+	transactionHeap(this).Swap(i, j)
 }
 
 // Push and Pop use pointer receivers because they modify slice's length
-func (this *TransactionQueue) Push(x interface{}) {
-	*this = append(*this, x.(Transaction))
+func (this *TransactionFIFO) Push(x interface{}) {
+	(*transactionHeap)(this).Push(x)
 }
-
-func (this *TransactionQueue) Pop() interface{} {
-	old := *this
-	n := len(old)
-	item := old[n-1]
-	*this = old[0 : n-1]
-	return item
+func (this *TransactionFIFO) Pop() interface{} {
+	return (*transactionHeap)(this).Pop()
 }
